06fe99993c86ad52d946be796d8b47218120e74f
shubham agrawal <manovagyanik1@gmail.com>
2017-07-11 03:05:33 +0530

    adding fb login for android and ios. for ios it is not yet working

--- app/index.js
+++ app/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import { Root, Tabs } from './config/router';
-import { Provider } from 'react-redux';
-import configureStore from "./store";
+import React, {Component} from 'react';
+import {Root, Tabs} from './config/router';
+import {Provider} from 'react-redux';
+import configureStore from './store';
 
 const store = configureStore();
 
-const AppRoot = ({ store }) => (
-    <Provider store={store}>
-        <Root/>
-    </Provider>
+const AppRoot = ({store}) => (
+	<Provider store={store}>
+		<Root />
+	</Provider>
 );
 
-const App = () => <AppRoot store={store}/>
+const App = () => <AppRoot store={store} />;
 
 export default App;
